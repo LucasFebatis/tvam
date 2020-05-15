1a31bff201d5b25982ea92879e667f95ba5b3ee5
Lucas Batista <lfebatis@gmail.com>
2020-05-15 01:48:19 -0300

    Implementando comando build

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -8,7 +8,22 @@ module.exports = {
   run: async (toolbox: GluegunToolbox) => {
     const {
       print: { info },
+      system
     } = toolbox
+
+    let yarnInstall = 'yarn install'
+    let yarnBuild = 'yarn build'
+    let cpDist = 'cp -av dist tizenProject'
+    let cdIn = 'cd tizenProject'
+    let tizenBuild = 'tizen package -t wgt -s emulator'
+
+    let result = await system.run(`${yarnInstall};${yarnBuild};${cpDist};${cdIn};${tizenBuild}`)
+    
+    info(result)
+    info(``)
+    info(`Se tudo ocorreu bem vc deve ter um novo wgt na pasta tizenProject esperando para ser instalado ou publicado`)
+    info(``)
+
     info(`build command executed`)
   },
 }
