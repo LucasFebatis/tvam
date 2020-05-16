5d096526721f2ffa95b3b4b7b138dfa580bf24f5
Lucas Batista <lfebatis@gmail.com>
2020-05-16 12:38:27 -0300

    cmd install: add suporte a webOSProject

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -1,6 +1,6 @@
 
 import { GluegunToolbox } from 'gluegun'
-  
+
 
 module.exports = {
   name: 'install',
@@ -9,26 +9,65 @@ module.exports = {
     const {
       print: { info },
       system,
-      parameters
+      parameters,
+      prompt
     } = toolbox
 
-    if(parameters.first) {
-      if(parameters.first.includes(".wgt")) {
+    if (parameters.first) {
+      if (parameters.first.includes(".wgt")) {
         let localInstall = `tizen install -n ${parameters.first} -- ./`
         let localResult = await system.run(`${localInstall}`)
         info(localResult)
         return
       }
+
+      else if (parameters.first.includes(".ipk")) {
+        let localInstall = `ares-install ${parameters.first}`
+        let localResult = await system.run(`${localInstall}`)
+        info(localResult)
+        return
+      }
+
+      else {
+        info("Arquivo não suportado")
+        return
+      }
+
     }
-    
-    let cdIn = 'cd tizenProject'
-    let createTizen = `tizen install -n tizenProject.wgt -- ./`
-    let result = await system.run(`${cdIn};${createTizen}`)
-    
-    info(result)
-    info(``)
-    info(`Se a instalação foi sucedida e o app não é mostrado, pode ser que vc precise reiniciar a tv ou o emulador`)
-    info(``)
+
+    const resultInstall = await prompt.ask([
+      {
+        type: 'select',
+        name: 'platform',
+        message: 'Install in',
+        choices: ['Tizen (Samsung)', 'Web OS (LG)'],
+      },
+    ])
+
+    if (resultInstall.platform == "Tizen (Samsung)") {
+
+      let cdIn = 'cd tizenProject'
+      let installTizen = `tizen install -n tizenProject.wgt -- ./`
+      await system.run(`${cdIn};${installTizen}`)
+
+      info(``)
+      info(`Se a instalação foi sucedida e o app não é mostrado, pode ser que vc precise reiniciar a tv ou o emulador`)
+      info(``)
+
+    }
+
+    if (resultInstall.platform == "Web OS (LG)") {
+
+      let cdIn = 'cd webOSProject'
+      let installwebOS = `ares-install com.domain.app_0.0.1_all.ipk`
+      await system.run(`${cdIn};${installwebOS}`)
+
+      info(``)
+      info(`Se a instalação foi sucedida e o app não é mostrado, pode ser que vc precise reiniciar a tv ou o emulador`)
+      info(``)
+
+    }
+
     info(`install command executed`)
   },
 }
